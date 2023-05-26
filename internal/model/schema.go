a28b776626b781cb30c5cfc1d641c3b5b0e758e1
jaroslav <jaroslav.shashunov@gmail.com>
2023-05-26 14:42:12 +0300

    add tables

--- internal/model/schema.go
+++ internal/model/schema.go
@@ -17,12 +17,30 @@ const (
     created_at timestamp,
     user_id bigint not null references users(id) 
 );`
+	createTrainingTable = `create table if not exists training (
+    id bigserial primary key,
+    words jsonb not null,
+    answers jsonb not null,
+    words_total bigint not null,
+    user_id bigint references users(id)
+);`
+	createStatisticTable = `create table if not exists statistic (
+    id bigserial primary key,
+    training_id bigint references training(id),
+    correct_answers jsonb,
+    wrong_answers jsonb,
+    user_id bigint references users(id)
+);`
 )
 
 func CreateTables(s *db.Storage) error {
 	tx := s.TX.MustBegin()
 	tx.MustExec(createUserTable)
 	tx.MustExec(createWordsTable)
+	tx.MustExec(createTrainingTable)
+	tx.MustExec(createStatisticTable)
 
 	return tx.Commit()
 }
+
+// TODO: implement creating training and statistic schemas
