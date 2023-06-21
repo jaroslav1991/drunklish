da011f174fa44c15e7f15445c52a5d3bb93909d7
jaroslav <jaroslav.shashunov@gmail.com>
2023-06-21 13:27:23 +0300

    fix logic responses

--- internal/pkg/httputils/httputils.go
+++ internal/pkg/httputils/httputils.go
@@ -8,7 +8,7 @@ import (
 )
 
 func WriteErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	response := map[string]any{"error": "internal server error", "path": r.URL.Path}
+	response := map[string]any{"error": "internal server error"}
 	code := http.StatusInternalServerError
 
 	if errors.Is(err, ErrValidation) {
@@ -35,18 +35,24 @@ func WriteErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 		response["error"] = err.Error()
 	}
 
-	log.Println(err, r.URL, code)
 	WriteSuccessResponse(w, r, code, response)
 }
 
 func WriteSuccessResponse(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
+
 	if data != nil {
 		response, err := json.Marshal(data)
 		if err != nil {
 			return
 		}
 		w.Write(response)
+
+		if code > 199 && code <= 299 {
+			log.Println(r.URL.Path, code)
+		} else {
+			log.Println(r.URL.Path, code, string(response))
+		}
 	}
 }
